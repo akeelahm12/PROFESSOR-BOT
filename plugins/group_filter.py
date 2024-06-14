1e5549f06012992fe9a5c5d6a091e47bf469ff52
PanjabiMoviePBX1 <bxjjjzid@gmail.com>
2024-06-15 02:02:39 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -104,7 +104,7 @@ async def next_page(bot, query):
     if 0 < offset <= 10:
         off_set = 0
     elif offset == 0:
-        off_set = None
+        off_set = true
     else:
         off_set = offset - 10
     if n_offset == 0:
